639e86c36a2152e8b0b1a9bbe6447d6e45a1d4fc
Abergel Clement <clement.abergel.1@ulaval.ca>
2021-04-03 10:22:49 -0400

    Parse ipt file

--- main.py
+++ main.py
@@ -1,2 +1,96 @@
+import collections
+
+from setuptools.dist import single_line
+
+
+class cmd_responses:
+
+    def __init__(self, line_count, col_count, drone_count, simulation_duration, max_drone_load, product_count,
+                 products_weight, warehouse_count, warehouse_info, cmd_count, commands_info):
+        self.line_count = line_count
+        self.col_count = col_count
+        self.drone_count = drone_count
+        self.simulation_duration = simulation_duration
+        self.max_drone_load = max_drone_load
+        self.product_count = product_count
+        self.products_weight = products_weight
+        self.warehouse_count = warehouse_count
+        self.warehouse_info = warehouse_info
+        self.cmd_count = cmd_count
+        self.commands_info = commands_info
+
+
+def parse_input_file(filename: str) -> cmd_responses:
+    file = open(filename, 'r')
+    lines = file.readlines()
+
+    line_count = 0
+    col_count = 0
+    drone_count = 0
+    simulation_duration = 0
+    max_drone_load = 0
+
+    first_line = lines[0].split(" ")
+
+    line_count = int(first_line[0])
+    col_count = int(first_line[1])
+    drone_count = int(first_line[2])
+    simulation_duration = int(first_line[3])
+    max_drone_load = int(first_line[4])
+
+    # print(line_count, col_count, drone_count, simulation_duration, max_drone_load)
+
+    product_count = int(lines[1])
+
+    third_line = lines[2].split(" ")
+    products_weight = []
+    for element in third_line:
+        products_weight.append(int(element))
+
+    # print(products_weight[:10])
+
+    warehouse_count = int(lines[3])
+    warehouse_info = []
+    line_index = 3 + warehouse_count * 2
+    for i in range(4, line_index, 2):
+        coords_string = lines[i].split(" ")
+        coords = (int(coords_string[0]), int(coords_string[1]))
+        qty = []
+        for element in lines[i+1].split(" "):
+            qty.append(int(element))
+
+        warehouse_info.append({
+            "coords": coords,
+            "qty": qty
+        })
+
+    # print(warehouse_info[:2][1])
+
+    line_index += 1
+    cmd_count = int(lines[line_index])
+    line_index += 1
+    commands_info = []
+    for i in range(line_index, line_index + cmd_count * 3, 3):
+        coords_string = lines[i].split(" ")
+        coords = (int(coords_string[0]), int(coords_string[1]))
+
+        nb_items = int(lines[i+1])
+        items = []
+        for element in lines[i+2].split(" "):
+            items.append(int(element))
+
+        dict_items_qty = collections.Counter(items)
+        commands_info.append({
+            "coords": coords,
+            "nb_items": nb_items,
+            "items_qty": dict_items_qty
+        })
+
+    # print(commands_info[-1])
+    return cmd_responses(line_count, col_count, drone_count, simulation_duration, max_drone_load, product_count,
+                         products_weight, warehouse_count, warehouse_info, cmd_count, commands_info)
+
+
 if __name__ == "__main__":
+    parse_input_file('maps/busy_day.in')
     print("Code here people :)")
